4e26ab0391df9437ac7b5cc0478b1b2f2a62a617
harveyimama <harveyimama@yahoo.com>
2020-08-27 19:22:30 +0100

    initial commit

---
+++ src/CardVerifier/src/main/java/com/mint/harvey/code/challenge/CardVerifier/CardVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.mint.harvey.code.challenge.CardVerifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardVerifierApplication.class, args);
+	}
+
+}
